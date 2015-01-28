9c84dd2b40386a42d6e84c85caec2360e1dafde2
LingjiaDeng <lingjiadeng@gmail.com>
2015-01-28 13:59:55 -0500

    Update DirectNode.java

--- src/readBishan/DirectNode.java
+++ src/readBishan/DirectNode.java
@@ -1,5 +1,18 @@
 package readBishan;
 
 public class DirectNode {
+  public String agent;
+  public String sentence;
+  public String span;
+  public ArrayList<String> targets;
+  public boolean overlapped;
+  
+  public DirectNode(){
+    this.agent = "";
+    this.sentence = "";
+    this.span = "";
+    this.targets = new ArrayList<String>();
+    this.overlapped = false;
+  }
 
 }
