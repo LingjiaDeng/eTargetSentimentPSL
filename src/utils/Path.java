0e9bd10be63e2d2f74b52d32384cb75a553988d6
LingjiaDeng <lingjiadeng@gmail.com>
2015-01-29 11:23:58 -0500

    temporarly move to laptop

--- src/utils/Path.java
+++ src/utils/Path.java
@@ -1,7 +1,8 @@
 package utils;
 
 public final class Path {
-	private static String root = "/afs/cs.pitt.edu/usr0/lid29/Documents/Bishan/";
+	//private static String root = "/afs/cs.pitt.edu/usr0/lid29/Documents/Bishan/";
+	private static String root = "/Users/Lingjia/Desktop/todo/sentimentPSL/";
 	
 	private Path(){
 	}
