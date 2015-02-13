b899fb322c977f33782b41e3578e3c9926a7f7a7
Lingjia DEng <lingjiadeng@gmail.com>
2015-02-12 23:22:48 -0500

    calculate ngram and store in feature class

--- src/structure/DirectNode.java
+++ src/structure/DirectNode.java
@@ -23,10 +23,7 @@ public class DirectNode {
 	public ArrayList<Tree> eTargetsGS;
 	public Tree root;
 	
-	public HashSet<String> unigramCon;
-	public HashSet<String> bigramCon;
-	public HashSet<String> unigramDep;
-	public HashSet<String> bigramDep;
+	public ArrayList<Feature> features;
 	
 	
 	public DirectNode(){
@@ -51,8 +48,13 @@ public class DirectNode {
 	}
 	
 	public void countItself(){
-		for (Tree eTarget:this.eTargets){
+		for (int t=0;t<this.eTargets;t++){
+			Tree eTarget = eTargets.get(t);
 			Feature feature = new Feature();
+			this.features.add(feature);
+			
+			
+			// itself
 			if (this.opinionTree.getLeaves().contains(eTarget)){
 				feature.inOpinionSpan = 1;
 			}
@@ -61,51 +63,49 @@ public class DirectNode {
 				if (target.contains(eTarget.nodeString()))
 					feature.inTargetSpan = 1;
 			}
-		}
-	}
-	
-	public void countCon(ArrayList<String> unigramCon, ArrayList<String> bigramCon){
-		int[] unigramCount = new int[unigramCon.size()];
-		int[] bigramCount = new int[bigramCon.size()];
-		
-		for (int i=0;i<unigramCon.size();i++){
-			if (this.unigramCon.contains(unigramCon.get(i)))
-				unigramCount[i] = 1;
-			else
-				unigramCount[i] = 0;
-		}
-		
-		for (int i=0;i<bigramCon.size();i++){
-			if (this.bigramCon.contains(bigramCon.get(i)))
-				bigramCount[i] = 1;
-			else
-				bigramCount[i] = 0;
-		}
-		
-		feature.unigramCon = unigramCount;
-		feature.bigramCount = bigramCount;
-	}
-	
-	public void countDep(ArrayList<String> unigramDep, ArrayList<String> bigramDep){
-		int[] unigramCount = new int[unigramDep.size()];
-		int[] bigramCount = new int[bigramDep.size()];
-		
-		for (int i=0;i<unigramDep.size();i++){
-			if (this.unigramDep.contains(unigramDep.get(i)))
-				unigramCount[i] = 1;
-			else
-				unigramCount[i] = 0;
-		}
-		
-		for (int i=0;i<bigramDep.size();i++){
-			if (this.bigramDep.contains(bigramDep.get(i)))
-				bigramCount[i] = 1;
-			else
-				bigramCount[i] = 0;
-		}
-		
-		feature.unigramDep = unigramCount;
-		feature.bigramDep = bigramDep;
+			
+			// constituency
+			feature.unigramCount = new int[unigramCon.size()];
+			feature.bigramCount = new int[bigramCon.size()];
+			
+			
+			for (int i=0;i<unigramCon.size();i++){
+				if (feature.unigramCon.contains(unigramCon.get(i)))
+					feature.unigramCount[i] = 1;
+				else
+					feature.unigramCount[i] = 0;
+			}
+			
+			for (int i=0;i<bigramCon.size();i++){
+				if (feature.bigramCon.contains(bigramCon.get(i)))
+					feature.bigramCount[i] = 1;
+				else
+					feature.bigramCount[i] = 0;
+			}
+			
+			//feature.unigramCon = unigramCount;
+			//feature.bigramCount = bigramCount;
+			
+			// dependency parser
+			unigramCount = new int[unigramDep.size()];
+			bigramCount = new int[bigramDep.size()];
+			
+			for (int i=0;i<unigramDep.size();i++){
+				if (feature.unigramDep.contains(unigramDep.get(i)))
+					unigramCount[i] = 1;
+				else
+					unigramCount[i] = 0;
+			}
+			
+			for (int i=0;i<bigramDep.size();i++){
+				if (feature.bigramDep.contains(bigramDep.get(i)))
+					bigramCount[i] = 1;
+				else
+					bigramCount[i] = 0;
+			}
+			
+			feature.unigramDep = unigramCount;
+			feature.bigramDep = bigramCount;
 	}
 
 }
